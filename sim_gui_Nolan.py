f900e247eb0eb53ca967bfb1db5fd20d1639aab3
14NDixon <nolan.dixon@usu.edu>
2021-07-16 09:42:24 -0600

    Updated PLDB Opt Color

--- sim_gui_Nolan.py
+++ sim_gui_Nolan.py
@@ -361,7 +361,8 @@ class BoomDataGUI:
                                                    pen=pldb_base_color, brush=pldb_base_color, symbol='d')
 
         # Create optimum PLdB plot
-        pldb_opt_color = "#FF5555"
+        # pldb_opt_color = "#FF5555"
+        pldb_opt_color = "#bd58e8"
         self._pldb_opt_axis = pg.AxisItem('left')
         self._pldb_opt_axis.setLabel('PL (optimum)', units='dB', color=pldb_opt_color)
         self._pldb_opt_view = pg.ViewBox()
